b22d48e94d34e5b7518dbaa0e19fe4a87230db86
Muhammad Sadit <muh.sadik25062006@gmail.com>
2025-09-09 18:36:54 +0800

    Days2.java

---
+++ Days2.java
@@ -0,0 +1,10 @@
+public class MuhammadSadi {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.print("Hello");
+        System.out.printf("\nnama %s","sadit");   
+        
+    }
+    
+  }
