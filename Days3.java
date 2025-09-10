71a15c40258a2c776d332e133b036d8d9e8321f4
Muhammad Sadit <muh.sadik25062006@gmail.com>
2025-09-10 13:37:08 +0800

    Days3.java

---
+++ Days3.java
@@ -0,0 +1,23 @@
+package Isc;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        // Comment Line, digunakan untuk membuat komentar dalam 1 baris
+        
+        /*
+        Multiline Comment, digunakan untuk membuat komentar lebih dari 1 baris
+        Contoh:
+        (baris 1)
+        (baris 2)
+        (dst.)
+        */
+        
+        //Komentar tidak akan dieksekusi
+        
+        //System.out.println("perintah ini tidak akan dieksekusi");
+        System.out.println("Sementara perintah ini akan dieksekusi");
+    }
+    
+}
