7caded0550dbdab563ea390f88185853d38c1515
Muhammad Sadit <muh.sadik25062006@gmail.com>
2025-09-11 20:07:42 +0800

    Day4.java

---
+++ Day4.java
@@ -0,0 +1,27 @@
+package Isc;
+
+public class Day4java {
+    
+    public static void main(String[] args) {
+        /*saya akan menjelas kan kode Escape Sequence berikut 5 kode Escape Sequence: 
+(\n ,\t,\r,\", \\,)*/
+        
+        
+    // kode (\n) digunakan untuk memindahkan teks ke garis baru
+        System.out.println("Hallo \n word");
+        
+    // kode (\t) di gunakan untuk mebuat tab atau spasi dan bisa juya merapikan teks
+        System.out.println("kelas \t: D");
+    
+    // kode (\r) berfungsi untuk menghilangkan teks dibelakang kode
+        System.out.println("sekolah \rdasar");
+        
+    //kode (\") menambah tanda kutip ganda didalam string
+        System.out.println("halo \"teman teman\" ");
+        
+    // kode (\\) berfungsi untuk menampilkan 1 backslash diteks
+        System.out.println("25\\07\\2006");
+        
+    }
+    
+}
