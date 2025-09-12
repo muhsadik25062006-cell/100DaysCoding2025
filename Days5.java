b65101f9ae460fe3870233028231580b08d0e6b9
Muhammad Sadit <muh.sadik25062006@gmail.com>
2025-09-12 22:32:53 +0800

    Days5.java

---
+++ Days5.java
@@ -0,0 +1,21 @@
+package Isc;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        //byte 8-bit (1 byte)
+        byte angka1 = 127;
+        //16-bit (2 byte)
+        short angka2 = 32767;
+        //32-bit (4 byte)
+        int angka3 = 2147483647;
+        //64-bit (8 byte)
+        long angka4 = 1234567890;
+        
+        System.out.println("angka1 = " + angka1);
+        System.out.println("angka2 = " + angka2);
+        System.out.println("angka3 = " + angka3);
+        System.out.println("angka4 = " + angka4);
+    }
+    
+}
