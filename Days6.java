4d78c9a5c065d23a027ef8fb9cdba4541abbaa1b
Muhammad Sadit <muh.sadik25062006@gmail.com>
2025-09-13 23:53:37 +0800

    Days6.java

---
+++ Days6.java
@@ -0,0 +1,19 @@
+package CodingIsc;
+public class Day6 {
+    public static void main(String[] args) {
+        
+               // Tipe Data Bilangan Pecahan(desimal) ada dua yaitu
+       
+       // yang pertama itu ada tipe data floating (float) harus pakai huruf 'f' di akhir angka
+       
+      float harga = 20.5f;
+        System.out.println("harga laptop Rog = "+ harga +"jt.");
+        
+        // yang kedua itu ada tipe data double floating pint (double)
+        
+        double inci = 16.0;
+        System.out.println("Lebar = "+ inci+ " inci");
+       
+    }
+    
+}
