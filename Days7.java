2f002b46b14f83fc137c8812da743fdc85885f20
Muhammad Sadit <muh.sadik25062006@gmail.com>
2025-09-14 16:55:29 +0800

    Days7.java

---
+++ Days7.java
@@ -0,0 +1,18 @@
+package CodingIsc ;
+public class Day7_java {
+    public static void main(String[] args) {
+        //Saya akan menjelaskan tipe data karakter (char), data tipe data (boolean).
+        //char Digunakan untuk menyimpan satu karakter saja, misalnya huruf, angka, atau simbol.Karakter dituliskan di dalam tanda petik tunggal ' ' → contoh: 'A', '7', '?'.
+        char Nilai = 'A';
+        System.out.println("Nilai rata rata : " +Nilai);
+        
+        // boolean untuk menentukan true/false.Tipe Data Boolean adalah tipe data yang hanya memiliki dua nilai logika, yaitu:
+        //true → bernilai benar
+        //false → bernilai salah
+        boolean Rajin = true;
+        System.out.println("Rajin \t\t: " +true );
+        boolean Malas = false;
+        System.out.println("Malas \t\t: " +false);
+        
+    }
+}
