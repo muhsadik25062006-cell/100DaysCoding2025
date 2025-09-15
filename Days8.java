d9c34395f6d252c20bc94194d595786c2b6d93e8
Muhammad Sadit <muh.sadik25062006@gmail.com>
2025-09-15 20:53:09 +0800

    Days8.java

---
+++ Days8.java
@@ -0,0 +1,9 @@
+package Isc;
+public class Day8m {
+    public static void main(String[] args) {
+        String Nama = "Muhammad Sadit";
+        String Jurusan = "Informatika";
+        System.out.println("Nama \t: " + Nama);
+        System.out.println("Jurusan : " + Jurusan);
+    }
+}
