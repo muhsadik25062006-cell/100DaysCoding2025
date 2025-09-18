6aaab20708303f62cda76f05a78d119c6b28e128
Muhammad Sadit <muh.sadik25062006@gmail.com>
2025-09-18 17:16:49 +0800

    Day11.java

---
+++ Day11.java
@@ -0,0 +1,23 @@
+package PemulaCoding ;
+import java.util.Scanner;
+public class Harike11 {
+    public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+        String nama, prodi;
+        String halo, perkenalkan;
+        System.out.print("nama : ");
+        nama = input.nextLine();
+        System.out.print("Prodi : ");
+        prodi = input.nextLine();
+        System.out.print("halo : ");
+        halo = input.nextLine();
+        System.out.print("perkenalkan :");
+        perkenalkan = input.nextLine();
+        
+        System.out.println("nama "+nama);
+        System.out.println("Prodi " + prodi);
+        
+        
+        
+    }
+}
