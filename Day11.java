8aad70f7df30d51d557cc3522a8cec47341730c8
Muhammad Sadit <muh.sadik25062006@gmail.com>
2025-09-18 17:59:42 +0800

    Day11.java

--- Day11.java
+++ Day11.java
@@ -1,23 +1,29 @@
 package PemulaCoding ;
 import java.util.Scanner;
-public class Harike11 {
+public class Harike4 {
+    
     public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
-        String nama, prodi;
-        String halo, perkenalkan;
-        System.out.print("nama : ");
-        nama = input.nextLine();
-        System.out.print("Prodi : ");
-        prodi = input.nextLine();
-        System.out.print("halo : ");
-        halo = input.nextLine();
-        System.out.print("perkenalkan :");
-        perkenalkan = input.nextLine();
-        
-        System.out.println("nama "+nama);
-        System.out.println("Prodi " + prodi);
-        
-        
-        
-    }
+        Scanner menyusun = new Scanner(System.in);
+        String halo;
+        String perkenalkan;
+        String nama;
+        String alamat;
+        int umur;
+        System.out.print("halo \t\t: ");
+        halo = menyusun.nextLine();
+        System.out.print("Perkenalkan \t: ");
+        perkenalkan = menyusun.nextLine();
+        System.out.print("nama \t\t: ");
+        nama = menyusun.nextLine();
+        System.out.print("alamat \t\t: ");
+        alamat = menyusun.nextLine();
+        System.out.print("umur \t\t: ");
+        umur = menyusun.nextInt();
+        System.out.println("================");
+        System.out.println("halo "+halo);
+        System.out.println("Perkenalkan " + perkenalkan);
+        System.out.println(nama + " Dari " + alamat);
+        System.out.println("Berumur "+umur+" tahun");
+    } 
+    
 }
