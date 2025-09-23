3ba148b69235e819274b9297c6cfe2173f5147a6
Muhammad Sadit <muh.sadik25062006@gmail.com>
2025-09-23 17:29:43 +0800

    Days16.java

---
+++ Days16.java
@@ -0,0 +1,16 @@
+package Isc;
+public class Days16 {
+    public static void main(String[] args) {
+        // Aritmatika perkalian
+        int a = 5;
+        int b = 5;
+        int hasil = a * b;
+        System.out.println("Hasil perkalian = " + hasil);
+        // Aritmatika pembagian
+        int x = 10;
+        int y = 5;
+        int Hasil = x / y;
+        System.out.println("Hasil pembagian = " + Hasil);
+        
+    }
+}
