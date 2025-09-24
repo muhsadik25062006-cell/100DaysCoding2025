d6bbb2b8205d538ec0b44ac13dc3d3434e5f89c8
Muhammad Sadit <muh.sadik25062006@gmail.com>
2025-09-24 09:42:06 +0800

    Days17.java

---
+++ Days17.java
@@ -0,0 +1,10 @@
+package Isc;
+public class Day17 {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 4;
+        int hasil = a %  b;
+        System.out.println("Hasil sisa bagi " + a + " % " + b + " = " + hasil);
+        
+    }
+}
