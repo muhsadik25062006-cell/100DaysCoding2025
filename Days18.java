f69aa309304d7a462794cf803c7ad4c20286c7b8
Muhammad Sadit <muh.sadik25062006@gmail.com>
2025-09-25 21:06:10 +0800

    Days18.java

---
+++ Days18.java
@@ -0,0 +1,20 @@
+package Isc;
+public class Day19 {
+    public static void main(String[] args) {
+        byte angka1 = 10;
+        short angka2 = angka1;
+        System.out.println(angka2);
+
+        short angka3 = 15;
+        int angka4 = angka3;
+        System.out.println(angka4);
+
+        int angka5 = 20;
+        long angka6 = angka5;
+        System.out.println(angka6);
+
+        float angka7 = 23.5f;
+        double angka8 = angka7;
+        System.out.println(angka8);
+    }
+}
