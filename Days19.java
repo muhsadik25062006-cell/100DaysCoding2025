f3bc1d67ace2b5e8ea1ddbe27ef6466c015891cc
Muhammad Sadit <muh.sadik25062006@gmail.com>
2025-09-26 22:41:56 +0800

    Days19.java

---
+++ Days19.java
@@ -0,0 +1,21 @@
+package Isc;
+public class Day19 {
+    public static void main(String[] args) {
+        // konversi manual
+        short x = 50;
+        byte y = (byte) x;
+        System.out.println("Nilai y : " + y);
+        
+        long a = 160;
+        int b = (int) a;
+        System.out.println("Nilai b : " + b);
+        
+        int c = 150;
+        short d = (short)c ;
+        System.out.println("Nilai d : " + d);
+        
+        double g = 25.5;
+        float h = (float)g;
+        System.out.println("Nilai h : " + h);
+    }
+}
