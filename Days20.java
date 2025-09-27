868fc3ee7b218f70cf7a598a43be1114e7477bd4
Muhammad Sadit <muh.sadik25062006@gmail.com>
2025-09-27 23:55:41 +0800

    Days20.java

---
+++ Days20.java
@@ -0,0 +1,19 @@
+package Isc;
+public class Days20 {
+    public static void main(String[] args) {
+        int in = 1000;
+        String  ind = String.valueOf(in);
+        System.out.println("INTEGER KE STRING = " + ind);
+        double Da = 18.89;
+        String Dan = String.valueOf(Da);
+        System.out.println("DOUBLE KE STRING  = " + Dan);
+        boolean Ba = true;
+        String Ban = String.valueOf(Ba);
+        System.out.println("BOOLEAN KE STRING = " + Ban);
+        
+        char Ka = 'A';
+        String Kak = String.valueOf(Ka);
+        System.out.println("CHAR KE STRING = " + Kak);
+        
+    }
+}
