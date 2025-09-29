4c48add6badae0534fccdacd9d4dfcf2f5747d6a
Muhammad Sadit <muh.sadik25062006@gmail.com>
2025-09-29 22:29:34 +0800

    Days22.java

---
+++ Days22.java
@@ -0,0 +1,11 @@
+package Isc;
+public class Day22 {
+    public static void main(String[] args) {
+        
+        int sisi = 5;
+        int luas = sisi * sisi;
+
+        System.out.println("Sisi persegi: " + sisi);
+        System.out.println("Luas persegi adalah: " + luas);
+    }
+}
