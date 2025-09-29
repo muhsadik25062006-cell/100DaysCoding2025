eb61332b2fd3280ea6c6bc58fc9a7ffc18c7cbc4
Muhammad Sadit <muh.sadik25062006@gmail.com>
2025-09-29 22:30:46 +0800

    Update Days22.java

--- Days22.java
+++ Days22.java
@@ -2,7 +2,7 @@ package Isc;
 public class Day22 {
     public static void main(String[] args) {
         
-        int sisi = 5;
+        int sisi = 4;
         int luas = sisi * sisi;
 
         System.out.println("Sisi persegi: " + sisi);
