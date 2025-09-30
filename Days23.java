37895ac5a9169f66a390bea8fd949d2b944e1591
Muhammad Sadit <muh.sadik25062006@gmail.com>
2025-09-30 20:59:33 +0800

    Days23.java

---
+++ Days23.java
@@ -0,0 +1,9 @@
+package Isc;
+public class Days23 {
+    public static void main(String[] args) {
+        int panjang = 10;
+        int lebar = 5;
+        int luas = panjang * lebar;
+        System.out.println("Luas persegi panjang = " + luas);
+    }
+}
