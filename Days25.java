a000f211fc2437cb020f25d5a5c2b4598c6f5ea4
Muhammad Sadit <muh.sadik25062006@gmail.com>
2025-10-02 21:01:26 +0800

    Days25.java

---
+++ Days25.java
@@ -0,0 +1,14 @@
+package Isc;
+public class Day25 {
+    public static void main(String[] args) {
+        //Increment
+        int x = 5;
+        x++; // sama dengan x = x + 1
+        System.out.println(" Nilai x = " + x);
+        
+        //Decrement
+        int y = 10;
+        y--; // sama dengan y = y - 1
+        System.out.println(" Nilai y = " + y);
+    }
+}
