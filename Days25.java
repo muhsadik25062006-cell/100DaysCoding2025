d90110a122a4187134d1721cd2a7a678d5222b18
Muhammad Sadit <muh.sadik25062006@gmail.com>
2025-10-02 21:25:52 +0800

    Days25.java

--- Days25.java
+++ Days25.java
@@ -3,12 +3,23 @@ public class Day25 {
     public static void main(String[] args) {
         //Increment
         int x = 5;
-        x++; // sama dengan x = x + 1
+        x++; 
         System.out.println(" Nilai x = " + x);
+        // pre-increment
+        int a = 5;
+        int b = ++x; 
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
         
         //Decrement
         int y = 10;
-        y--; // sama dengan y = y - 1
+        y--; // 
         System.out.println(" Nilai y = " + y);
+        // pre-decrement
+        int c = 5;
+        int d = --x; 
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+
     }
 }
