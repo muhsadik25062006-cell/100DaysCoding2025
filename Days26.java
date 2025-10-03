52241f8914fce87e6aaa0a44acf8fbfc6f4232e2
Muhammad Sadit <muh.sadik25062006@gmail.com>
2025-10-03 08:29:25 +0800

    Days26.java

---
+++ Days26.java
@@ -0,0 +1,28 @@
+package Isc;
+public class Day26 {
+    public static void main(String[] args) {
+        //Sama dengan
+        int x = 10;   
+        System.out.println(" Nilak akhir x = "+ x);
+        //Tambah sama dengan)
+        int y = 10;
+        y +=5;
+        System.out.println(" Nilai akhir y = " + y);
+        //Kurang sama dengan)
+        int a = 20;
+        a -= 7;
+        System.out.println(" Nilai akhir a = " + a);
+        //Kali sama dengan)
+        int b = 4;
+        b *=3;
+        System.out.println(" Nilai akhir b = " + b);
+        //Bagi sama dengan)
+        int c = 50;
+        c /=5;
+        System.out.println(" Nilai Akhir c = " + c);
+        //Modulos atau sisa bagi sama dengan)
+        int d = 10;
+        d %= 3;
+        System.out.println(" Nilai Akhir d = " + d);
+    }
+}
