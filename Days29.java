42aa6ed909f5540196350fbd576ef342a0ce6830
Muhammad Sadit <muh.sadik25062006@gmail.com>
2025-10-06 21:57:02 +0800

    Days29.java

--- Days29.java
+++ Days29.java
@@ -1,15 +1,10 @@
 package Isc;
-import java.util.Scanner;
 public class day29 {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int a, b;
-        System.out.print(" a : "); 
-        a = in.nextInt();
-        System.out.print(" b : "); 
-        b = in.nextInt();
-
+        int a,b;
+        a = 10;
+        b = 5;
         System.out.println(a > b); 
-        System.out.println(a < b); 
+        System.out.println(a < b);
     }
 }
