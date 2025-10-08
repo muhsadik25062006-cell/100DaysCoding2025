5324874810e81ee3326b84e09a0f769211d6699f
Muhammad Sadit <muh.sadik25062006@gmail.com>
2025-10-08 22:09:23 +0800

    Days31.java

---
+++ Days31.java
@@ -0,0 +1,10 @@
+package Isc;
+public class Day31 {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+        boolean hasil = (x < y) && (x > 5);
+        
+        System.out.println("Apakah x < y dan x > 5? " + hasil);
+    }
+}
