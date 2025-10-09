4dc61183107a2652e0380e44ac7332e5b2ba06ee
Muhammad Sadit <muh.sadik25062006@gmail.com>
2025-10-09 21:45:53 +0800

    Days32.java

---
+++ Days32.java
@@ -0,0 +1,9 @@
+package Isc;
+public class Day32 {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 25;
+        boolean hasil = (a > 20) || (b > 20);
+        System.out.println("Apakah x > 15 atau y > 15? " + hasil);
+    }
+}
