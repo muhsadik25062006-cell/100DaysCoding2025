9c9c283173bcb39e4a69b41e6dfc07fa6f2a99c1
Muhammad Sadit <muh.sadik25062006@gmail.com>
2025-10-13 22:45:12 +0800

    Days36.java

--- Days36.java
+++ Days36.java
@@ -1,16 +1,18 @@
 package Isc;
 public class Days36 {
     public static void main(String[] args) {
-        int nilai = 89;
+        int nilai = 70;
         if (nilai >= 75) {
             System.out.println(" Lulus");
-        if (nilai >= 85 && nilai <= 100) {
+        if (nilai >= 90 && nilai <= 100) {
             System.out.println(" Dapat peringkat 1");
-        } else if (nilai >= 75 && nilai <= 84) {
+        } else if (nilai >= 80 && nilai <= 89) {
             System.out.println(" Dapat peringkat 2");
+    }else if (nilai >= 75 && nilai <80){
+        System.out.println(" Dapat peringkat 3");
     }
- }else if (nilai <= 74){
+ }else if (nilai <= 74)
      System.out.println(" Tidak Dapat peringkat");
  }
     }
-}
+
