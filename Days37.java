0a9040fc4794748d03bd277553c8f95ba574a587
Muhammad Sadit <muh.sadik25062006@gmail.com>
2025-10-14 22:02:16 +0800

    Days37.java

---
+++ Days37.java
@@ -0,0 +1,11 @@
+package Isc;
+public class Days37 {
+    public static void main(String[] args) {
+        int angka = 10;
+        if (angka %2==0){
+            System.out.println("angka " + angka + " adalah bilangan genap");
+        }else if (angka %2==1){
+            System.out.println(angka + " bilangan ganjil");
+        }
+    }
+}
