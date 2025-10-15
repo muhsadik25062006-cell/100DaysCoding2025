e38e0b4579d2d45afbe220ca9f325269182a56c1
Muhammad Sadit <muh.sadik25062006@gmail.com>
2025-10-15 21:09:39 +0800

    Days38.java

---
+++ Days38.java
@@ -0,0 +1,13 @@
+package Isc;
+public class Day38 {
+    public static void main(String[] args) {
+        int angka = 5;
+        if (angka > 0) {
+            System.out.println("angka " + angka + " adalah bilangan positif.");
+        } else if (angka < 0) {
+            System.out.println("angka " + angka + " adalah bilangan negatif.");
+        } else {
+            System.out.println("Bilangan tersebut adalah nol.");
+        }
+    }
+}
