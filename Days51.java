0d101ec089d6dd581aacec4eb545f94ad59626ab
Muhammad Sadit <muh.sadik25062006@gmail.com>
2025-10-28 20:57:23 +0800

    Days51.java

---
+++ Days51.java
@@ -0,0 +1,8 @@
+package Isc; 
+public class Day50 {
+    public static void main(String[] args) {
+        for(int a = 1; a <= 8; a++){
+            System.out.println(" Angka :" + a);
+        }
+    }
+}
