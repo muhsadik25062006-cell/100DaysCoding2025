7782bd08cb6ee275e969904c1d804d871dc192ce
Muhammad Sadit <muh.sadik25062006@gmail.com>
2025-10-29 19:59:04 +0700

    Days52.java

---
+++ Days52.java
@@ -0,0 +1,13 @@
+package coding100days;
+import java.util.Scanner;
+public class harike52 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner (System.in);
+        int awal = 1;
+        int ahir = 10;
+        while(awal <= ahir ){
+            System.out.println("angka : " + awal);
+            awal++;
+        }
+    }
+}
