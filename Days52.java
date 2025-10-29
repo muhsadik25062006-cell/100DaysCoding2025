b36548b0644f5bc49f28424b9ed2cc47729975b7
Muhammad Sadit <muh.sadik25062006@gmail.com>
2025-10-29 21:59:52 +0800

    Days52.java
    Removed unused Scanner import from Days52.java

--- Days52.java
+++ Days52.java
@@ -1,8 +1,6 @@
 package coding100days;
-import java.util.Scanner;
 public class harike52 {
     public static void main(String[] args) {
-        Scanner in = new Scanner (System.in);
         int awal = 1;
         int ahir = 10;
         while(awal <= ahir ){
