51368d97cb30de198a18942f104f3b04b4060d00
Muhammad Sadit <muh.sadik25062006@gmail.com>
2025-10-30 22:21:28 +0800

    Days53.java

---
+++ Days53.java
@@ -0,0 +1,12 @@
+package Isc;
+public class harike53 {
+    public static void main(String[] args) {
+         for(int i = 1; i <= 15; i++){
+            System.out.println(" Angka: " + i);
+            if(i == 10){
+                System.out.println(" perulangan dihentikan di angka 10");
+                break; 
+            }
+        }
+    }
+}
