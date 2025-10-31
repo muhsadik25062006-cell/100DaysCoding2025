11148c6cce4b99a4ea3322dc6a47eaa0b6d7b7fb
Muhammad Sadit <muh.sadik25062006@gmail.com>
2025-10-31 20:32:27 +0700

    Days54.java

---
+++ Days54.java
@@ -0,0 +1,11 @@
+package coding100days;
+public class harike54 {
+    public static void main(String[] args) {       
+        for (int i = 0; i < 10; i++) {
+            if (i == 3){
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}
