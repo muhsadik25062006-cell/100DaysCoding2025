2b0991c277a9134e6b0479908e1d5233bfb44697
Muhammad Sadit <muh.sadik25062006@gmail.com>
2025-11-02 20:46:26 +0700

    Days56.java with a do-while loop example

---
+++ Days56.java
@@ -0,0 +1,10 @@
+package coding100days;
+public class harike56 {
+    public static void main(String[] args) {
+        int a = 1;
+         do {
+             System.out.println("lagi 44 hari" );
+             a++;
+        } while (a <= 5);
+    }
+}
