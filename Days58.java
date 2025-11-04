d57480c0ffbe95d22c39c1b01480e9d043745e7a
Muhammad Sadit <muh.sadik25062006@gmail.com>
2025-11-04 20:30:17 +0700

    Days58.java to print numbers 1 to N and N to 1

---
+++ Days58.java
@@ -0,0 +1,16 @@
+package coding100days;
+public class harike58 {
+    public static void main(String[] args) {     
+        int N = 10;      
+        System.out.print(" Angka dari 1 ke N : ");
+        for (int i = 1; i <= N; i++) {
+            System.out.print(i + " ");
+        }      
+        System.out.println();      
+        System.out.print(" Angka dari N ke 1 : ");
+        for (int i = N; i >= 1; i--) {
+            System.out.print(i + " ");
+        }
+        System.out.println(" ");
+    }
+}
