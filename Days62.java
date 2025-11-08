03d9d2bfd8ff61045b4a1bb306143ecbb281600a
Muhammad Sadit <muh.sadik25062006@gmail.com>
2025-11-08 19:48:43 +0700

    java to calculate sum of numbers

---
+++ Days62.java
@@ -0,0 +1,14 @@
+package coding100days;
+import java.util.Scanner;
+public class harike62 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner (System.in);
+        System.out.print("Masukan N : ");
+        int N = in.nextInt();
+        int total = 0;
+        for (int i = 1; i <= N; i++) {
+            total += i;
+        }
+        System.out.println("jumlah dari 1 sampai " + N + " adalah : " + total);
+    }
+}
