7852a17cb7653788418dadc5b74b60353209f19b
Muhammad Sadit <muh.sadik25062006@gmail.com>
2025-11-17 21:26:48 +0700

    pola persegi tiga siku siku

--- Days71.java
+++ Days71.java
@@ -1,10 +1,10 @@
 package coding100days;
-public class harike72 {
+public class harike71 {
     public static void main(String[] args) {
         int n = 5;
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.print(" * ");
+                System.out.print("*  ");
             }
             System.out.println();
         }
