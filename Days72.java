1ec9f6c58274344e2c06a06183f4e888446439e1
Muhammad Sadit <muh.sadik25062006@gmail.com>
2025-11-17 23:31:11 +0700

    pola

---
+++ Days72.java
@@ -0,0 +1,12 @@
+package coding100days;
+public class harike72 {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
