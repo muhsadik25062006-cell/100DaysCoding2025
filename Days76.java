bea762c18514802fafbbfd92e4778d8ad77f2916
Muhammad Sadit <muh.sadik25062006@gmail.com>
2025-11-22 22:04:52 +0700

    hari ke 77

---
+++ Days76.java
@@ -0,0 +1,14 @@
+package coding100days;
+public class harike76 {
+    public static void main(String[] args) {
+        String a = "Belajar";
+        String b = "belajar";
+        String c = "belajar Java";
+        String d = "no";
+
+        System.out.println(a.equals(b));
+        System.out.println(a.equalsIgnoreCase(b));
+        System.out.println(c.contains(b));
+        System.out.println(d.isEmpty());
+    }
+}
