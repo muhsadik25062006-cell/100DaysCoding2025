e0bb63250b99200830ce60c5b38fc24ebe0aef85
Muhammad Sadit <muh.sadik25062006@gmail.com>
2025-11-26 02:37:22 +0700

    Array 1D

---
+++ Days80.java
@@ -0,0 +1,11 @@
+package coding100days;
+public class harike80 {
+    public static void main(String[] args) {
+        int [] angka = {1,2,3,4,5};
+        System.out.print("Angka : ");
+        for (int i = 0; i < angka.length; i++) {
+            System.out.print(angka[i] + " " );     
+        }
+        System.out.println();
+    }
+}
