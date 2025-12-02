27516097338e824e4bd8d63bfa9858050a880669
Muhammad Sadit <muh.sadik25062006@gmail.com>
2025-12-02 19:45:59 +0700

    menentukan angka minimum dalam array

--- Days86.java
+++ Days86.java
@@ -1,11 +1,11 @@
 package coding100days;
 public class harike86 {
     public static void main(String[] args) {
-        int [] angka = {10,30,20,100,80,50};
-        int min = angka[0];
-        for (int i = 0; i < angka.length; i++) {
-            if (angka[i] < min) {
-                min = angka[i];
+        int [] angkaArray = {10,30,20,100,80,50};
+        int min = angkaArray[0];
+        for (int i = 0; i < angkaArray.length; i++) {
+            if (angkaArray[i] < min) {
+                min = angkaArray[i];
             }
         }
         System.out.println("Nilai minimal : " + min);
