ac2f64096a9ac4175ef004bd7a5818ae313952ed
Muhammad Sadit <muh.sadik25062006@gmail.com>
2025-12-04 23:22:27 +0800

    mengakses elemen array menggunakan for each

---
+++ Days88.java
@@ -0,0 +1,10 @@
+package coding100days;
+public class harike88 {
+    public static void main(String[] args) {   
+        char[] array = {'S','a','d','i','t'}; 
+        for (char b : array ) {
+            System.out.print(b+ " ");
+        }  
+        System.out.println();
+    }
+}
