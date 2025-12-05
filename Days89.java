7b08677460d3502aae78881df72468678fc6d1a4
Muhammad Sadit <muh.sadik25062006@gmail.com>
2025-12-05 21:48:00 +0800

    Method void

---
+++ Days89.java
@@ -0,0 +1,9 @@
+package coding100days;
+public class harike89 {
+    public static void teks() {
+        System.out.println("Pemula");
+    }
+    public static void main(String[] args) {
+        teks();
+    }
+}
