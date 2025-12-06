33c8a39a1ec3f2c0c369260619c95497532bd700
Muhammad Sadit <muh.sadik25062006@gmail.com>
2025-12-06 23:16:26 +0800

    method non-void

---
+++ Days90.java
@@ -0,0 +1,9 @@
+package coding100days;
+public class harike90 {
+    public static String biodata(String nama,String asal,int umur) {
+        return "Perkenalkan saya " + nama + " Saya berasal dari " + asal + " umur " + umur;
+    }
+    public static void main(String[] args) {
+        System.out.println(biodata("Naruto","Konoha",19));
+    }
+}
