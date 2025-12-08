be2d5cc294522f7a0f3d17992ce00cf819d81bc5
Muhammad Sadit <muh.sadik25062006@gmail.com>
2025-12-08 20:17:33 +0800

    Keyword return

---
+++ Days92.Java
@@ -0,0 +1,12 @@
+package coding100days;
+public class harike92 {
+    public static int tambah(int a, int b) {
+        int hasil = a + b;
+        return hasil;
+    }
+    public static void main(String[] args) {
+        int hasil = tambah(5, 3);
+        System.out.println("Hasil penjumlahan: " + hasil);
+    }
+}
+   
