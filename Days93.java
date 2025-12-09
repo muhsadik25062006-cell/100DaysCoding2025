6aa2b1750d8b1900ff314dcf67b60a82a8628869
Muhammad Sadit <muh.sadik25062006@gmail.com>
2025-12-09 21:59:12 +0800

    pembulatan ceil,floor dan round

--- Days93.java
+++ Days93.java
@@ -1,13 +1,12 @@
 package coding100days;
-public class harike94 {
-    public static void main(String[] args) {      
-        double angka = 7.7;
+public class harike93 {
+    public static void main(String[] args) {
+         double angka = 7.7;
         
         double ceil = Math.ceil(angka);
         double floor = Math.floor(angka);
         double round = Math.round(angka);
 
         System.out.printf("angka awal : %.1f \nhasil ceil : %.1f \nhasil floor : %.1f \nhasil round : %.0f \n",angka,ceil,floor,round);
-        
     }
 }
