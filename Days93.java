2865edaec1305c4c2b710a61fa9a8ca54082883c
Muhammad Sadit <muh.sadik25062006@gmail.com>
2025-12-09 21:56:42 +0800

    math ceil,floor dan round

---
+++ Days93.java
@@ -0,0 +1,13 @@
+package coding100days;
+public class harike94 {
+    public static void main(String[] args) {      
+        double angka = 7.7;
+        
+        double ceil = Math.ceil(angka);
+        double floor = Math.floor(angka);
+        double round = Math.round(angka);
+
+        System.out.printf("angka awal : %.1f \nhasil ceil : %.1f \nhasil floor : %.1f \nhasil round : %.0f \n",angka,ceil,floor,round);
+        
+    }
+}
