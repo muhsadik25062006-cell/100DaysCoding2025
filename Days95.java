f5dc9706ab49181aa0a06b6fe0eb508be785bd48
Muhammad Sadit <muh.sadik25062006@gmail.com>
2025-12-11 21:25:17 +0800

    mencetak dengan parameter void

--- Days95.java
+++ Days95.java
@@ -1,8 +1,8 @@
 package coding100days;
 import java.util.Scanner;
 public class harike95 {
-    public static void kataKata(String kata ) {
-        System.out.println(kata);
+    public static void kataKata(String kata) {
+        System.out.println("Kata kata hari ini : " + kata);
     }
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
